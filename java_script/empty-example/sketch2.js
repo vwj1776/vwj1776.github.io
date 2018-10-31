04d73a2c33eaa3f53b60d9e1728b0726efebd831
Unknown <vwj1776@gmail.com>
2018-10-30 17:22:39 -0700

    trying with processing

--- java_script/empty-example/sketch2.js
+++
@@ -1,8 +0,0 @@
-fill(255, 0, 255);
-
-draw = function() {
-    background(255, 255, 255);
-    ellipse(mouseX, mouseY, 1, 1); 
-    var label = mouseX +" , "+ mouseY;
-    text(label, mouseX +10, mouseY +10);
-};
